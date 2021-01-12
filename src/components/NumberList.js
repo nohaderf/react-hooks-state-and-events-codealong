333e313b9929aec26a6c5ba9533523aed92aba5e
nohaderf <freda.hon@gmail.com>
2021-01-11 22:09:49 -0500

    commit tree 8c822e7ba0a76d5cb3637469397ebaeb99569193

--- src/components/NumberList.js
+++ src/components/NumberList.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import { randomNumber } from "../utils";
 
-function Numbers() {
+function NumberList() {
+
+  const [numbers, setNumbers] = useState([1,2,3])
+  const numberList = numbers.map((number) => <li key={number}>{number}</li>)
+
   function handleAddNumber() {
     const newNumber = randomNumber();
+    const newNumberArray = [...number, newNumber]
     console.log(newNumber);
   }
+
+  function handleLiClick(numberToRemove){
+    // const newNumberArray = numbers.filter((number) => number !== numberToRemove)
+    // setNumbers(newNumberArray)
+
+    const newNumberArray = numbers.map((number) => {
+      if (number === numberToRemove) {
+        return numberToRemove + 100 ;
+      } else {
+        return number
+      }
+    })
+    setNumbers(newNumberArray)
+  }
+
 
   return (
     <div>
       <button onClick={handleAddNumber}>Add Number</button>
-      <ul>{/* list of numbers */}</ul>
+      <ul>{numberList}</ul>
     </div>
   );
 }
